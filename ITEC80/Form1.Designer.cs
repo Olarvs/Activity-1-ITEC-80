722a23fa43a17c2986ff967cd14e3621175d7d87
unknown <manuelolarve@outlook.ph>
2022-10-25 22:41:30 +0800

    Added Functionality

--- ITEC80/Form1.Designer.cs
+++ ITEC80/Form1.Designer.cs
@@ -28,13 +28,303 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtFulllName = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtSection = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.txtFili = new System.Windows.Forms.TextBox();
+            this.txtEng = new System.Windows.Forms.TextBox();
+            this.txtSci = new System.Windows.Forms.TextBox();
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.txtHisto = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFulllName
+            // 
+            this.txtFulllName.AutoSize = true;
+            this.txtFulllName.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtFulllName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFulllName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtFulllName.Location = new System.Drawing.Point(41, 30);
+            this.txtFulllName.Name = "txtFulllName";
+            this.txtFulllName.Size = new System.Drawing.Size(119, 24);
+            this.txtFulllName.TabIndex = 0;
+            this.txtFulllName.Text = "Full Name:";
+            this.txtFulllName.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Green;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUpdate.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnUpdate.Location = new System.Drawing.Point(15, 419);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(388, 36);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Submit";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(214, 124);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(216, 20);
+            this.txtFName.TabIndex = 3;
+            this.txtFName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // txtSection
+            // 
+            this.txtSection.AutoSize = true;
+            this.txtSection.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtSection.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSection.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtSection.Location = new System.Drawing.Point(66, 75);
+            this.txtSection.Name = "txtSection";
+            this.txtSection.Size = new System.Drawing.Size(94, 24);
+            this.txtSection.TabIndex = 4;
+            this.txtSection.Text = "Section:";
+            this.txtSection.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(139, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subjects";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(106, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DCIT 25";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(106, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ITEC 100";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(105, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "ITEC 111";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(104, 391);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 24);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "ITEC 80";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(104, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 24);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "DCIT 65";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtSec
+            // 
+            this.txtSec.Location = new System.Drawing.Point(214, 175);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(216, 20);
+            this.txtSec.TabIndex = 11;
+            // 
+            // txtFili
+            // 
+            this.txtFili.Location = new System.Drawing.Point(214, 256);
+            this.txtFili.Name = "txtFili";
+            this.txtFili.Size = new System.Drawing.Size(216, 20);
+            this.txtFili.TabIndex = 12;
+            // 
+            // txtEng
+            // 
+            this.txtEng.Location = new System.Drawing.Point(214, 292);
+            this.txtEng.Name = "txtEng";
+            this.txtEng.Size = new System.Drawing.Size(216, 20);
+            this.txtEng.TabIndex = 13;
+            // 
+            // txtSci
+            // 
+            this.txtSci.Location = new System.Drawing.Point(214, 328);
+            this.txtSci.Name = "txtSci";
+            this.txtSci.Size = new System.Drawing.Size(216, 20);
+            this.txtSci.TabIndex = 14;
+            // 
+            // txtMath
+            // 
+            this.txtMath.Location = new System.Drawing.Point(214, 364);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(216, 20);
+            this.txtMath.TabIndex = 15;
+            // 
+            // txtHisto
+            // 
+            this.txtHisto.Location = new System.Drawing.Point(214, 397);
+            this.txtHisto.Name = "txtHisto";
+            this.txtHisto.Size = new System.Drawing.Size(216, 20);
+            this.txtHisto.TabIndex = 16;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblResult.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblResult.Location = new System.Drawing.Point(104, 441);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(89, 24);
+            this.lblResult.TabIndex = 17;
+            this.lblResult.Text = "Student";
+            this.lblResult.Visible = false;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblGPA.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGPA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblGPA.Location = new System.Drawing.Point(104, 475);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(56, 24);
+            this.lblGPA.TabIndex = 18;
+            this.lblGPA.Text = "GPA";
+            this.lblGPA.Visible = false;
+            this.lblGPA.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.txtSection);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtFulllName);
+            this.panel1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(27, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(431, 489);
+            this.panel1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "GPA Generator";
+            this.label1.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(524, 621);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtHisto);
+            this.Controls.Add(this.txtMath);
+            this.Controls.Add(this.txtSci);
+            this.Controls.Add(this.txtEng);
+            this.Controls.Add(this.txtFili);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "Form1";
+            this.Text = "StudentData";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtFulllName;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label txtSection;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.TextBox txtFili;
+        private System.Windows.Forms.TextBox txtEng;
+        private System.Windows.Forms.TextBox txtSci;
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.TextBox txtHisto;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ITEC80/Form1.cs
+++ ITEC80/Form1.cs
@@ -16,5 +16,115 @@ namespace ITEC80
         {
             InitializeComponent();
         }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string result = "";
+
+            result = txtFName.Text + " " + txtSec.Text;
+
+            lblResult.Text = result;
+
+            /* txtFili
+             * txtMath
+             * txtHisto
+             * txtSci
+             * txtEng
+             */
+
+            result = txtFName.Text + " " + txtSec.Text;
+
+            float gpa = float.Parse(txtFili.Text) + float.Parse(txtMath.Text) + float.Parse(txtHisto.Text) + float.Parse(txtSci.Text) + float.Parse(txtEng.Text);
+
+            lblResult.Text = result;
+            gpa /= 5;
+
+
+            lblGPA.Text = "Your GPA is = " + gpa.ToString("0.00");
+
+            MessageBox.Show("Hello" + result + "\n" + "Your GPA is " + gpa);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+    
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
     }
 }
